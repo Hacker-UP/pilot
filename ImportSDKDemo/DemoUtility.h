dc9ed22e883a109832f1c310859c038cf355df6d
Desgard_Duan <desgard_duan@foxmail.com>
2016-10-15 06:11:56 +0800

    ADD: update Camera model, can get the webcam video

--- ImportSDKDemo/DemoUtility.h
+++ ImportSDKDemo/DemoUtility.h
@@ -12,6 +12,21 @@
 #define WeakRef(__obj) __weak typeof(self) __obj = self
 #define WeakReturn(__obj) if(__obj ==nil)return;
 
+#ifndef DemoUtility_h
+#define DemoUtility_h
+
+#import "DemoUtilityMacro.h"
+#import "DemoUtilityMethod.h"
+#import "DemoAlertView.h"
+#import "DemoComponentHelper.h"
+#import "DemoGetSetViewController.h"
+#import "DemoPushInfoViewController.h"
+#import "DemoSettingItem.h"
+#import "DemoTableViewController.h"
+#import "MBProgressHUD.h"
+
+#endif /* DemoUtility_h */
+
 @class DJIBaseProduct;
 @class DJIAircraft;
 @class DJIGimbal;
